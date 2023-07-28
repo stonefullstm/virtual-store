7b3b7ca01d519c63b83313bae3c4ff6e7205b280
stonefullstm <stonefull.stm@gmail.com>
2023-07-28 16:32:14 -0300

    feat: Criação da rota POST /accounts para criar uma conta

--- app/routes/accounts.py
+++ app/routes/accounts.py
@@ -2,15 +2,28 @@ from fastapi import APIRouter, Depends
 from sqlmodel import Session, select
 
 from app.models.account import Account
-from app.schemas.accounts import AccountList
+from app.schemas.accounts import AccountList, AccountPublic
 from app.backend.db import get_session
 
 router = APIRouter(prefix='/accounts')
 
 
-@router.get('/', response_model=AccountList)
+@router.get('/', response_model=AccountList, tags=['accounts'])
 def read_accounts(
     session: Session = Depends(get_session)
 ):
     accounts = session.exec(select(Account)).all()
     return {'accounts': accounts}
+
+
+@router.post('/',
+             response_model=AccountPublic,
+             status_code=201, tags=['accounts'])
+def create_account(
+    account: Account,
+    session: Session = Depends(get_session)
+):
+    session.add(account)
+    session.commit()
+    session.refresh(account)
+    return account
