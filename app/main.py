e4f67da3e3cc2dac2534e0503eb8792e644a6a53
stonefullstm <stonefull.stm@gmail.com>
2023-07-28 19:33:55 -0300

    feat: Definindo configuração de CORS

--- app/main.py
+++ app/main.py
@@ -1,9 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.backend.db import create_db_and_tables
 
 from app.routes import accounts
 
+origins = ['*']
+
 app = FastAPI()
+
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins=origins,
+  allow_credentials=True,
+  allow_methods=['*'],
+  allow_headers=['*'],
+)
 
 
 @app.on_event("startup")
