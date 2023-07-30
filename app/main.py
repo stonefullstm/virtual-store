f86a6c1b6f1ad3baa4007db2557a4bec053e3139
stonefullstm <stonefull.stm@gmail.com>
2023-07-30 16:16:31 -0300

    docs: Customização da documentação swagger

--- app/main.py
+++ app/main.py
@@ -2,13 +2,45 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
+from fastapi.openapi.utils import get_openapi
 from app.backend.db import create_db_and_tables
 
 from app.routes import accounts
 
 origins = ['*']
 
-app = FastAPI()
+app = FastAPI(
+    title="Virtual Store API",
+    description="This API implements store transaction system.",
+    version="1.0.0",
+    )
+
+
+def custom_openapi():
+    if not app.openapi_schema:
+        app.openapi_schema = get_openapi(
+            title=app.title,
+            version=app.version,
+            openapi_version=app.openapi_version,
+            description=app.description,
+            terms_of_service=app.terms_of_service,
+            contact=app.contact,
+            license_info=app.license_info,
+            routes=app.routes,
+            tags=app.openapi_tags,
+            servers=app.servers,
+        )
+        for _, method_item in app.openapi_schema.get('paths').items():
+            for _, param in method_item.items():
+                responses = param.get('responses')
+                # remove 422 response, also can remove other status code
+                if '422' in responses:
+                    del responses['422']
+    return app.openapi_schema
+
+
+app.openapi = custom_openapi
+
 
 app.add_middleware(
   CORSMiddleware,
--- app/routes/accounts.py
+++ app/routes/accounts.py
@@ -5,10 +5,27 @@ from app.models.account import Account
 from app.schemas.accounts import AccountPublic, AccountList, AccountSchema
 from app.backend.db import get_session
 
+
+error_response = {
+                400: {
+                    "description": "Validation Error.",
+                    "content": {
+                        "application/json": {
+                            "example": {
+                                "message": "Validation error on /name/ field"}
+                            }
+                    },
+                },
+             }
+
 router = APIRouter(prefix='/accounts')
 
 
-@router.get('/', response_model=AccountList, tags=['accounts'])
+@router.get(
+        '/',
+        summary='Returns all accounts.',
+        response_model=AccountList,
+        tags=['Accounts'])
 def read_accounts(
     session: Session = Depends(get_session)
 ):
@@ -17,8 +34,11 @@ def read_accounts(
 
 
 @router.post('/',
+             summary='Create an account.',
              response_model=AccountPublic,
-             status_code=201, tags=['accounts'])
+             status_code=201,
+             responses=error_response,
+             tags=['Accounts'])
 def create_account(
     account: AccountSchema,
     session: Session = Depends(get_session)
